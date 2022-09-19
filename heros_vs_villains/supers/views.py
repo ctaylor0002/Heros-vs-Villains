460f302ddd42ba284f6ae9c6cf2264b05fcfd637
cTaylorDFF <ctaylor@dffcorp.com>
2022-09-19 14:37:09 -0400

    Created Serializer for Supers + Created basic GET and POST Request API Handlers

--- heros_vs_villains/supers/views.py
+++ heros_vs_villains/supers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+#from .serializers import SuperSerializer
+from .models import Supers
+#from supers import serializers
 
-# Create your views here.
+
+@api_view(['GET','POST'])
+def supers_list(request):
+    
+    if request.method == 'GET':
+        pass
+    
